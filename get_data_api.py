280251cd35cdb8da39055f55c0f73315ce8a175f
Kulakov <akulakov544@gmail.com>
2024-06-26 13:44:08 +0300

    закончил часть Ñпс парсером

--- get_data_api.py
+++ get_data_api.py
@@ -17,7 +17,7 @@ def get_catalog_code_car(vin_car):
     for attr in data_catalog_namber_car["commonAttributes"]:
         if attr['key'] == "Catalog":
             catalog_namber_car = attr['value']
-    #print(catalog_namber_car)
+
     return catalog_namber_car
 
 
@@ -33,6 +33,7 @@ def get_car_info(catalog_namber_car):
             'model': item['name'],
             'release_date': item['date'][0:4]
         }
+
     return car_info
 
 
@@ -42,26 +43,62 @@ def get_car_details(catalog_namber_car):
     response = requests.get(url_search_car_details)
     data_car_details = json.loads(response.text)
     car_details = []
-    for item in data_car_details["data"]:
-        car_details.append(item['name'])
-    print(car_details)
+
+    for item in data_car_details["data"][0]['children']:
+        car_details.append({
+            'name': item['name'],
+            "quickGroupId": item['quickGroupId']
+        })
+
     return car_details
+
+
+def get_articl_details(catalog_namber_car, quickGroupId):
+    url_search_car_articl = f'https://catalogoriginal.autodoc.ru/api/catalogs/original/brands/{catalog_namber_car}/cars/0/quickgroups/{quickGroupId}/units'
+    # Данные для отправки в запросе POST
+    payload = {
+        'ssd': ssd,
+    }
+    # Выполнение POST-запроса
+    response = requests.post(url_search_car_articl, json=payload)
+
+    # Проверка статуса ответа
+    if response.status_code == 200:
+        try:
+            data_car_articl = response.json()
+            # Массив для хранения нужных данных
+            car_articl = []
+
+            # Обработка данных
+            i = 0
+            for item in data_car_articl["items"]:
+                car_articl.append({'code': item['code'], 'name': item['name'], 'details_info': []})
+                for details_info in item["spareParts"]:
+                    if 'match' in details_info:
+                        car_articl[i]['details_info'].append({'name': details_info['name'], 'partNumber': details_info['partNumber']})
+                i = i+1
+
+            return car_articl
+
+        except ValueError as e:
+            print(f"Ошибка при декодировании JSON: {e}")
+    else:
+        print(f"Ошибка: {response.status_code}")
+        print("Текст ответа:", response.text)
+        if response.status_code == 400:
+            print("Ошибка 400: Неверный запрос. Проверьте параметры и данные запроса.")
+
+
 
 
 def get_data_car(vin_car):
     catalog_namber_car = get_catalog_code_car(vin_car)
-    car_infо = get_car_info(catalog_namber_car)
-    car_details = get_car_details(catalog_namber_car)
-    print(car_infо)
 
-    response = requests.get(url)
-    # Проверка статуса ответа
-    if response.status_code == 200:
-        # Преобразование ответа в формат JSON
-        data = response.json()
-        return car_infо, car_details
-    else:
-        print(f"Ошибка: {response.status_code}")
+    car_infо = get_car_info(catalog_namber_car)
+
+    car_details = get_car_details(catalog_namber_car)
+
+    return car_infо, car_details, catalog_namber_car
 
 
 # vin_car = 'WAUBH54B11N111054'
