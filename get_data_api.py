5553090c9b4e96ed0d60ec5a315328b64d26815b
Kulakov <akulakov544@gmail.com>
2024-07-02 14:05:22 +0300

    ÐУбрал глобальныеГлобальные переменные, обработка ошибокÐ

--- get_data_api.py
+++ get_data_api.py
@@ -4,16 +4,14 @@ from loguru import logger
 
 
 logger.add("debag.log", format="{time} {level} {message}")
-# URL API
-url = ""
-ssd_car = ''
+
 
 def get_catalog_code_car(vin_car):
     '''Функция
     :param vin_car: vin омер полученный от пользователя
     :return catalog_namber_car, ssd_car: номер машины в каталоге, токен передающийся с запросом
     '''
-    global catalog_namber_car, ssd_car
+
     url_search_catalog_namber_car = f'https://catalogoriginal.autodoc.ru/api/catalogs/original/cars/{vin_car}/modifications?clientId=375'
     response = requests.get(url_search_catalog_namber_car)
 
@@ -38,7 +36,11 @@ def get_catalog_code_car(vin_car):
         if response.status_code == 400:
             return 0, 400
             #"Ошибка 400: Неверный запрос. Проверьте параметры и данные запроса."
-        return 0, 0
+        elif response.status_code == 404:
+            return 0, 404
+        elif response.status_code == 503:
+            return 0, 503
+    return 0, 0
 
 
 def get_car_info(catalog_namber_car, ssd_car):
@@ -46,7 +48,6 @@ def get_car_info(catalog_namber_car, ssd_car):
     :param catalog_namber_car, ssd_car: номер машины в каталоге
     :return car_info: информация о машине
     '''
-    global car_info
     url_search_car_info = f'https://catalogoriginal.autodoc.ru/api/catalogs/original/catalogCodes/{catalog_namber_car}?ssd={ssd_car}'
     response = requests.get(url_search_car_info)
 
@@ -67,7 +68,6 @@ def get_car_details(catalog_namber_car, ssd_car):
     :param catalog_namber_car: номер машины в каталоге
     :return car_info: информация о машине
     '''
-    global car_details
     url_search_car_details = f'https://catalogoriginal.autodoc.ru/api/catalogs/original/brands/{catalog_namber_car}/cars/0/quickgroups?ssd={ssd_car}'
     response = requests.get(url_search_car_details)
     data_car_details = json.loads(response.text)
@@ -115,23 +115,6 @@ def get_articl_details(catalog_namber_car, quickGroupId, ssd_car):
         except ValueError as e:
             # "Ошибка при декодировании JSON
             return 0
-    else:
-        # Могут быть разные ошибки
-        if response.status_code == 400:
-            return 400
-            # "Ошибка 400: Неверный запрос. Проверьте параметры и данные запроса."
-        return 0
-
-
-def get_data_car(catalog_namber_car, ssd_car):
-    '''Функция
-    :param catalog_namber_car, ssd_car: описание
-    :return car_infо, car_details: что возвращает
-    '''
-    car_infо = get_car_info(catalog_namber_car, ssd_car)
-    car_details = get_car_details(catalog_namber_car, ssd_car)
-
-    return car_infо, car_details
 
 
 #Дальше идет код только для тестирования
@@ -141,7 +124,8 @@ def test_function(vin_car, quickGroupId):
     logger.debug("catalog_namber_car: ", catalog_namber_car, "\nssd: ", ssd_car)
     print("catalog_namber_car: ", catalog_namber_car, "\nssd: ", ssd_car)
 
-    car_info, car_details = get_data_car(catalog_namber_car, ssd_car)
+    car_info = get_car_info(catalog_namber_car, ssd_car)
+    car_details = get_car_details(catalog_namber_car, ssd_car)
     logger.debug("car_info: ", car_info)
     print("car_info: ", car_info)
     print("car_details: ", car_details)
@@ -152,6 +136,6 @@ def test_function(vin_car, quickGroupId):
 
 
 # quickGroupId = 2
-# vin_car = 'Z8NAJL00050366148'
+# vin_car = 'VF1LA0H5324321010'
 #
 # test_function(vin_car, quickGroupId)
